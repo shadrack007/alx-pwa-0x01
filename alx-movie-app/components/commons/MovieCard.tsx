5f1979093a9c9bd4445a8587421e09380e95629a
shadrack007 <shadrack.h.mwakibinga@gmail.com>
2025-06-21 00:29:15 +0300

    feat: implemented MovirCard

--- alx-movie-app/components/commons/MovieCard.tsx
+++ alx-movie-app/components/commons/MovieCard.tsx
@@ -1,7 +1,27 @@
-const MovieCard: React.FC = () => {
+import { MovieProps } from "@/interfaces";
+import Image from "next/image";
+
+const MovieCard: React.FC<MovieProps> = ({
+  title,
+  posterImage,
+  releaseYear,
+}) => {
   return (
-    <div>
-      <p>movie card</p>
+    <div className="h-[563px]">
+      <div>
+        <Image
+          src={posterImage}
+          className="hover:cursor-pointer h-[430px] w-full rounded-md"
+          alt={title}
+          width={100}
+          height={100}
+        />
+      </div>
+
+      <div className="flex justify-between py-4">
+        <p className="text-xl font-bold">{title}</p>
+        <p className="text-xl text-[#e2d609]">{releaseYear}</p>
+      </div>
     </div>
   );
 };
