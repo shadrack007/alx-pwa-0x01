94b96d288e36b3a4563078d67ceda573d4e6c282
shadrack007 <shadrack.h.mwakibinga@gmail.com>
2025-06-20 21:16:02 +0300

    feat: implemented button

--- alx-movie-app/components/commons/Button.tsx
+++ alx-movie-app/components/commons/Button.tsx
@@ -1,5 +1,14 @@
-const Button: React.FC = () => {
-  return <button>button</button>;
+import { ButtonProps } from "@/interfaces";
+
+const Button: React.FC<ButtonProps> = ({ title, action }) => {
+  return (
+    <button
+      onClick={action}
+      className="px-8 py-2 border-2 border-[#e2d609] rounded-full hover:bg-[#e2d609] hover:text-black transition-colors duration-300"
+    >
+      {title}
+    </button>
+  );
 };
 
 export default Button;
