0f157517632bd5d0bd3a031c67c0d0b322eb5caa
shadrack007 <shadrack.h.mwakibinga@gmail.com>
2025-06-21 00:16:54 +0300

    feat: implemented MovieProps interface

--- alx-movie-app/interfaces/index.ts
+++ alx-movie-app/interfaces/index.ts
@@ -8,3 +8,22 @@ export interface ButtonProps {
   title: string;
   action?: () => void;
 }
+
+interface PrimaryImage {
+  url: string;
+}
+
+interface TitleText {
+  text: string;
+}
+
+interface ReleaseYear {
+  year: string;
+}
+
+export interface MovieProps {
+  id: string;
+  primaryImage: PrimaryImage;
+  titleText: TitleText;
+  releaseYear: ReleaseYear;
+}
