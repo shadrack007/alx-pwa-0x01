d4b1aae67890c367290a40d6c0a905c61bb3f5c6
shadrack007 <shadrack.h.mwakibinga@gmail.com>
2025-06-20 21:16:49 +0300

    feat: implemented header component

--- alx-movie-app/components/layouts/Header.tsx
+++ alx-movie-app/components/layouts/Header.tsx
@@ -1,7 +1,46 @@
+import Link from "next/link";
+
+import Button from "../commons/Button";
+
 const Header: React.FC = () => {
   return (
-    <header>
-      <p>header</p>
+    <header className="h-28 flex items-center px-4 md:px-16 lg:px-44 text-white bg-[#171d22]">
+      <div className="flex items-center justify-between w-full">
+        <h2 className="text-xl md:text-4xl font-semibold">
+          Cine <span className="text-[#e2d609]">Seek</span>
+        </h2>
+
+        <nav className="hidden md:flex flex-1 justify-center space-x-8">
+          <Link
+            href="/"
+            className="hover:text-[#e2d609] px-4 md:px-8 text-xl transition-colors duration-300 font-semibold"
+          >
+            Home
+          </Link>
+
+          <Link
+            href="/movies"
+            className="hover:text-[#e2d609] px-4 md:px-8 text-xl transition-colors duration-300 font-semibold"
+          >
+            Home
+          </Link>
+
+          <Link
+            href="/contact"
+            className="hover:text-[#e2d609] px-4 md:px-8 text-xl transition-colors duration-300 font-semibold"
+          >
+            Contact
+          </Link>
+        </nav>
+
+        <div className="flex md:hidden">
+          <Button title="Sign in" />
+        </div>
+
+        <div className="hidden md:flex">
+          <Button title="Sign in" />
+        </div>
+      </div>
     </header>
   );
 };
