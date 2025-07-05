0c0bfe3756c0d2d92ab8f5bb9166f406c4a42a06
shadrack007 <shadrack.h.mwakibinga@gmail.com>
2025-07-05 18:50:06 +0300

    configured PWA

--- alx-movie-app/next.config.ts
+++ alx-movie-app/next.config.ts
@@ -1,4 +1,9 @@
 import type { NextConfig } from "next";
+import withPWAInit from "@ducanh2912/next-pwa";
+
+const withPWA = withPWAInit({
+  dest: "public",
+});
 
 const nextConfig: NextConfig = {
   /* config options here */
@@ -10,7 +15,10 @@ const nextConfig: NextConfig = {
         hostname: "**", // allow all hostnames
       },
     ],
+    domains: ["m.media-amazon.com"],
   },
 };
 
-export default nextConfig;
+export default withPWA({
+  ...nextConfig,
+});
